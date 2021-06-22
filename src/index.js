5457541f71325b06b6286943bb5f7b18835de7ea
Jacob Klimenko <droidedjake@gmail.com>
2021-06-21 21:35:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(e) {
+    const inputField = document.getElementById("new-task-description");
+    userInput = inputField.value;
+    li = document.createElement("li");
+    li.textContent = userInput;
+    
+    list = document.querySelector("div#list ul#tasks");
+    list.appendChild(li);
+    form.reset();
+    e.preventDefault();
+  });
 });
